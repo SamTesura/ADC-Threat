ab801ca4f859ba3061749ab27361d4486071794d
SamTesura <68511853+SamTesura@users.noreply.github.com>
2025-10-24 17:10:09 -0400

    Update adc-list.js

--- adc-list.js
+++ adc-list.js
@@ -1,40 +1,41 @@
 /* ============================================================
    ADC_LIST — Updated 2025
-   Contains all modern ADC champions for grid rendering
+   Contains all modern ADC champions for grid rendering.
+   Now using direct DDragon image links.
    ============================================================ */
 
 const ADC_LIST = [
   // Core traditional ADCs
-  { name: "Ashe", image: "img/ashe.png" },
-  { name: "Caitlyn", image: "img/caitlyn.png" },
-  { name: "Jinx", image: "img/jinx.png" },
-  { name: "Ezreal", image: "img/ezreal.png" },
-  { name: "Kai'Sa", image: "img/kaisa.png" },
-  { name: "Miss Fortune", image: "img/missfortune.png" },
-  { name: "Xayah", image: "img/xayah.png" },
-  { name: "Aphelios", image: "img/aphelios.png" },
-  { name: "Tristana", image: "img/tristana.png" },
-  { name: "Sivir", image: "img/sivir.png" },
-  { name: "Lucian", image: "img/lucian.png" },
-  { name: "Samira", image: "img/samira.png" },
-  { name: "Vayne", image: "img/vayne.png" },
-  { name: "Draven", image: "img/draven.png" },
-  { name: "Varus", image: "img/varus.png" },
-  { name: "Zeri", image: "img/zeri.png" },
-  { name: "Nilah", image: "img/nilah.png" },
-  { name: "Smolder", image: "img/smolder.png" },
+  { name: "Ashe", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Ashe.png" },
+  { name: "Caitlyn", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Caitlyn.png" },
+  { name: "Jinx", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Jinx.png" },
+  { name: "Ezreal", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Ezreal.png" },
+  { name: "Kai'Sa", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Kaisa.png" },
+  { name: "Miss Fortune", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/MissFortune.png" },
+  { name: "Xayah", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Xayah.png" },
+  { name: "Aphelios", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Aphelios.png" },
+  { name: "Tristana", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Tristana.png" },
+  { name: "Sivir", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Sivir.png" },
+  { name: "Lucian", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Lucian.png" },
+  { name: "Samira", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Samira.png" },
+  { name: "Vayne", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Vayne.png" },
+  { name: "Draven", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Draven.png" },
+  { name: "Varus", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Varus.png" },
+  { name: "Zeri", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Zeri.png" },
+  { name: "Nilah", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Nilah.png" },
+  { name: "Smolder", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Smolder.png" },
 
   // Hybrid or flex ADCs
-  { name: "Twitch", image: "img/twitch.png" },
-  { name: "Kog'Maw", image: "img/kogmaw.png" },
-  { name: "Kalista", image: "img/kalista.png" },
+  { name: "Twitch", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Twitch.png" },
+  { name: "Kog'Maw", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/KogMaw.png" },
+  { name: "Kalista", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Kalista.png" },
   
   // Mage / APCs (common in high elo)
-  { name: "Senna", image: "img/senna.png" }, // Often played as "fasting" ADC
-  { name: "Seraphine", image: "img/seraphine.png" },
-  { name: "Veigar", image: "img/veigar.png" },
-  { name: "Ziggs", image: "img/ziggs.png" },
-  { name: "Karthus", image: "img/karthus.png" },
-  { name: "Swain", image: "img/swain.png" },
-  { name: "Heimerdinger", image: "img/heimerdinger.png" },
+  { name: "Senna", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Senna.png" }, // Often played as "fasting" ADC
+  { name: "Seraphine", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Seraphine.png" },
+  { name: "Veigar", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Veigar.png" },
+  { name: "Ziggs", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Ziggs.png" },
+  { name: "Karthus", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Karthus.png" },
+  { name: "Swain", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Swain.png" },
+  { name: "Heimerdinger", image: "http://ddragon.leagueoflegends.com/cdn/14.14.1/img/champion/Heimerdinger.png" }
 ];
